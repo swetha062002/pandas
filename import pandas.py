c602db4259c75787aecb4b13a42038fcf5fb8a7f
swetha062002 <101513669+swetha062002@users.noreply.github.com>
2022-03-27 19:35:24 +0530

    Add files via upload

---
+++ import pandas.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  2 20:30:27 2022
+
+@author: happy
+"""
+import pandas as pd
+
+path=r"C:/Users/happy/Documents/ott.xlsx"
+data=pd.read_excel(path)
+print(data)
+
+data.OTT.mean()
+data.THEATRE.mean()
+data.OTT.hist()
+data.OTT.describe()
+data.OTT.value_counts()
+data.THEATRE.describe()
+data.THEATRE.value_counts()
+data.OTT.unique()
+data.THEATRE.unique()
